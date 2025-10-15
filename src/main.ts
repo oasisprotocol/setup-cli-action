a5f5730db8f8435883033c38c57e410b16e3c645
Anže Luzar <aluzarwork@gmail.com>
2025-10-15 15:39:51 +0200

    WIP: action

---
+++ src/main.ts
@@ -0,0 +1,46 @@
+import * as core from '@actions/core'
+import { wait } from './wait.js'
+
+const core = require('@actions/core');
+const tc = require('@actions/tool-cache');
+
+async function setup() {
+  // Get version of tool to be installed
+  const version = core.getInput('version');
+
+  // Download the specific version of the tool, e.g. as a tarball
+  const pathToTarball = await tc.downloadTool(getDownloadURL());
+
+  // Extract the tarball onto the runner
+  const pathToCLI = await tc.extractTar(pathToTarball);
+
+  // Expose the tool by adding it to the PATH
+  core.addPath(pathToCLI)
+}
+
+module.exports = setup
+
+/**
+ * The main function for the action.
+ *
+ * @returns Resolves when the action is complete.
+ */
+export async function run(): Promise<void> {
+  try {
+    const ms: string = core.getInput('milliseconds')
+
+    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
+    core.debug(`Waiting ${ms} milliseconds ...`)
+
+    // Log the current timestamp, wait, then log the new timestamp
+    core.debug(new Date().toTimeString())
+    await wait(parseInt(ms, 10))
+    core.debug(new Date().toTimeString())
+
+    // Set outputs for other workflow steps to use
+    core.setOutput('time', new Date().toTimeString())
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
